e116ec7dadfdd987f59f491bba745bf0184852d6
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-03-02 21:32:43 -0300

    feature: getting parameters

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
+  const { page, limit } = request.query;
+  console.log(page);
+  console.log(limit);
   return response.json(["NodeJs", "NestJs", "AdonisJs", "Express"]);
 });
 
 app.post("/courses", (request, response) => {
+  const { course } = request.body;
+  console.log(course);
   return response.json(["NodeJs", "NestJs", "AdonisJs", "Express", "GraphQl"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json([
     "NodeJs",
     "NestJs",
@@ -21,14 +30,14 @@ app.put("/courses/:id", (request, response) => {
 });
 
 app.patch("/courses/:id", (request, response) => {
-    return response.json([
-      "NodeJs",
-      "NestJs",
-      "AdonisJs",
-      "ExpressJs",
-      "GraphQL",
-    ]);
-  });
+  return response.json([
+    "NodeJs",
+    "NestJs",
+    "AdonisJs",
+    "ExpressJs",
+    "GraphQL",
+  ]);
+});
 
 app.delete("/courses/:id", (request, response) => {
   return response.json(["NodeJs", "NestJs", "AdonisJs", "ExpressJs"]);
